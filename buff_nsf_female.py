a662ec396f02f23ce84dae8db6b895f2bb1ce670
Yuxuan Xue <yuxuan.xue@outlook.com>
2023-08-02 22:42:53 +0200

    update nsf with animation from aist

--- buff_nsf_female.py
+++ buff_nsf_female.py
@@ -46,6 +46,56 @@ from pytorch3d.io import save_obj, save_ply
 from models.losses import chamfer_distance_s2m_m2s
 
 class Trainer(Basic_Trainer_nsf):
+
+    def animate(self, save_animation, poses_file_path, pretrained=None, checkpoint=None):
+        epoch = self.load_checkpoint(path=pretrained, number=checkpoint)
+
+        print('Animating with epoch {}'.format(epoch))
+
+        poses_file = np.load(poses_file_path, allow_pickle=True).item()
+        pose_all = poses_file['pose']
+        transl_all = poses_file['transl']
+        assert len(pose_all) == len(transl_all) 
+
+        device = self.device
+
+        with torch.no_grad():
+            self.set_feat_training_mode(train_flag=False)
+
+            for subj_garment in tqdm(self.val_dataset.subject_index_dict.keys()):
+                feature_cube_idx = torch.tensor(self.val_dataset.subject_index_dict[subj_garment])[None].to(device)
+
+                for n in tqdm(range(len(pose_all))):
+                    device = self.device
+
+                    pose = torch.from_numpy(pose_all[n])[None].to(device)
+
+                    smpl_d_mesh = self.nsf_feature_surface.smpl_d_dense_mesh.to(self.device)[feature_cube_idx]
+                    x_useless_sampled, _ = sample_points_from_meshes(smpl_d_mesh, num_samples=30000, return_normals=True)
+
+                    coarse_corr = x_useless_sampled.permute(0, 2, 1).contiguous()
+
+                    inputs = {'coarse_corr': coarse_corr,
+                        'pose': pose,
+                        'trans': torch.from_numpy(transl_all[n]).to(device),
+                        'feature_cube_idx': feature_cube_idx}
+
+                    logits = self.predict(inputs, train=False)
+
+                    pred_cano_cloth_verts = logits['cano_cloth_points'].permute(0, 2, 1).contiguous()[:, 30000:]
+                    pred_posed_cloth_verts = logits['posed_cloth_points'].permute(0, 2, 1).contiguous()[:, 30000:]
+                    # get updated mesh
+                    faces_new = self.nsf_feature_surface.smpl_d_dense_mesh.faces_padded().to(device)[feature_cube_idx]
+
+                    subject = subj_garment.split('_')[0] 
+                    garment = subj_garment.split('_')[1] 
+                    save_folder = join(self.exp_path, save_animation + '_ep_{}'.format(epoch), subject, garment, str(n))
+
+                    if not os.path.exists(save_folder):
+                        os.makedirs(save_folder)
+
+                    save_ply(save_folder+'/posed_mesh.ply', verts=pred_posed_cloth_verts[0], faces=faces_new[0])
+                    save_ply(save_folder+'/cano_mesh.ply', verts=pred_cano_cloth_verts[0], faces=faces_new[0])
     
     # test on seen subjects but unseen subject
     def test_model(self, save_name, num_samples=-1, pretrained=None, checkpoint=None):
@@ -89,6 +139,15 @@ class Trainer(Basic_Trainer_nsf):
                 # get updated mesh
                 faces_new = self.nsf_feature_surface.smpl_d_dense_mesh.faces_padded().to(device)[feature_cube_idx]
 
+                add_root_rotation = True
+                if add_root_rotation:
+                    pose_root = batch.get('pose_root').to(device)
+                    sw_root = torch.zeros((pred_posed_cloth_verts.shape[0], 24, pred_posed_cloth_verts.shape[1])).to(device).float()
+                    sw_root[:, 0, :] = 1
+                    trans_root = torch.tensor([0]).cuda().float()
+                    # skin the root rotation
+                    pred_posed_cloth_verts_rot = self.skinner(pred_posed_cloth_verts.permute(0, 2, 1).contiguous(), pose_root, sw_root, trans_root)['posed_cloth_points'].permute(0,2,1).contiguous()
+
                 names = batch.get('path')
 
                 for i in range(len(names)):
@@ -105,6 +164,9 @@ class Trainer(Basic_Trainer_nsf):
 
                     save_ply(save_folder+'/posed_mesh.ply', verts=pred_posed_cloth_verts[i], faces=faces_new[i])
                     save_ply(save_folder+'/cano_mesh.ply', verts=pred_cano_cloth_verts[i], faces=faces_new[i])
+
+                    if add_root_rotation:
+                        save_ply(save_folder+'/posed_mesh_rot.ply', verts=pred_posed_cloth_verts_rot[i], faces=faces_new[i])
 
 
     def predict(self, batch, animate=False, train=True):
@@ -325,4 +387,17 @@ if __name__ == "__main__":
         trainer = Trainer(module_dict, pretrained_module_dict, device=torch.device("cuda"), val_dataset=val_dataset, exp_name=exp_name)
 
         trainer.test_model(args.save_name)
+        
+    if args.mode == 'fine_tune':
 
+        val_dataset = DataLoader_Buff_depth(mode='val', nsf_cano_available=True, batch_size=args.batch_size, num_workers=4, split_file=args.split_file, subject_index_dict=subject_index_dict, num_points=30000)  
+        trainer = Trainer(module_dict, pretrained_module_dict, device=torch.device("cuda"), val_dataset=val_dataset, exp_name=exp_name)
+
+        trainer.fine_tune_model(args.epochs)
+
+    if args.mode == 'animate':
+
+        val_dataset = DataLoader_Buff_depth(mode='val', nsf_cano_available=True, batch_size=args.batch_size, num_workers=4, split_file=args.split_file, subject_index_dict=subject_index_dict, num_points=30000)  
+        trainer = Trainer(module_dict, pretrained_module_dict, device=torch.device("cuda"), val_dataset=val_dataset, exp_name=exp_name)
+
+        trainer.animate('animate_'+args.save_name, poses_file_path='data_animation/aist_poses.npy')
